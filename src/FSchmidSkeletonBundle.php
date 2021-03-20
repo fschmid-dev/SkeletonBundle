e8a20ab592e75510a9a31e2c1424336a789153d0
TheOnlySpeedy <speedyschmid@gmx.net>
2021-03-20 22:04:27 +0100

    Add Bundle and TestKernel clsses

---
+++ src/FSchmidSkeletonBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace FSchmid\SkeletonBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FSchmidSkeletonBundle extends Bundle
+{
+
+}
--- tests/TestKernel.php
+++ tests/TestKernel.php
@@ -4,7 +4,47 @@
 namespace FSchmid\SkeletonBundle\Tests;
 
 
-class TestKernel
-{
+use FSchmid\SkeletonBundle\FSchmidSkeletonBundle;
+use Generator;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 
+class TestKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    /**
+     * @return Generator|iterable|BundleInterface[]
+     */
+    public function registerBundles(): Generator
+    {
+        $bundles = [
+            FrameworkBundle::class,
+            FSchmidSkeletonBundle::class
+        ];
+
+        foreach ($bundles as $bundle) {
+            yield new $bundle();
+        }
+    }
+
+    public function getCacheDir(): string
+    {
+        return __DIR__.'/cache/'.spl_object_hash($this);
+    }
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        // TODO: Implement configureRoutes() method.
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        // TODO: Implement configureRoutes() method.
+    }
 }
