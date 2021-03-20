b8460f61268f5cd9c671fd62719da1ff0a2604d9
TheOnlySpeedy <speedyschmid@gmx.net>
2021-03-20 22:54:11 +0100

    Add necessery files

---
+++ src/DependencyInjection/FSchmidSkeletonExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace FSchmid\SkeletonBundle\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class FSchmidSkeletonExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+}
